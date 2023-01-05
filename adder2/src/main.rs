5552a62ad40fd84577558d677763e5e5f75d2a65
leslie <hzzz2yb@gmail.com>
2023-01-05 23:38:32 +0800

    first up

---
+++ adder2/src/main.rs
@@ -0,0 +1,8 @@
+use add_one;
+use add_two::add_two;
+use add_three::add_three;
+fn main() {
+    println!("{}",add_one::add_one(8));
+    println!("{}",add_two(66));
+    println!("{}",add_three(73));
+}
