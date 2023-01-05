cdd9a8094ffe522277692ffda70e8ba6af9b8eb6
HZzz2 <hzzz2yb@gmail.com>
2023-01-05 23:52:26 +0800

    add_two增加平方计算

--- add_two/src/lib.rs
+++ add_two/src/lib.rs
@@ -2,6 +2,10 @@ pub fn add_two(n:i32)->i32{
     n+2
 }
 
+pub fn square_two(n:i32)->i32{
+    n*n
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -14,5 +18,13 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_square_two(){
+        //测试平方
+        assert_eq!(square_two(2),4);
+        assert_eq!(square_two(4),16);
+        assert_eq!(square_two(5),25);
     }
 }
