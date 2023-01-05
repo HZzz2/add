f801cfa9bcaf40d209b2c8082681e3e52d659446
HZzz2 <hzzz2yb@gmail.com>
2023-01-05 23:45:22 +0800

    adder bin

--- adder/src/main.rs
+++ adder/src/main.rs
@@ -1,3 +1,8 @@
+use add_one;
+use add_two::add_two;
+use add_three::add_three;
 fn main() {
-    println!("Hello, world!");
+    println!("{}",add_one::add_one(8));
+    println!("{}",add_two(66));
+    println!("{}",add_three(82));
 }
